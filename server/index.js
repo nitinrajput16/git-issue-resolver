e03f3150ad3f73776f557e8c1f1c8e3450963a96
shivam kumar <nr750001@gmail.com>
2026-03-19 15:55:40 +0530

    deployed

--- server/index.js
+++ server/index.js
@@ -57,8 +57,8 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     },
   })
